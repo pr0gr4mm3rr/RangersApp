96f61d1d02547437b7b1271e6dfb7d63ede063db
Jared Miller <jarmille@umflint.edu>
2019-08-10 17:14:04 -0400

    Updated Paths

---
+++ precache-manifest.b2284472bad9ad0eda0ba70e76720d10.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c93b2e285ae5e10a04",
+    "url": "index.bundle.js"
+  },
+  {
+    "revision": "fa49c11d47f9dc46f577a401d739132e",
+    "url": "index.html"
+  }
+]);
