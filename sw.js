e794e6afe672006434a782c2209fbeeb2af55070
Jared Miller <jarmille@umflint.edu>
2019-08-10 16:17:57 -0400

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,3 @@
+importScripts("precache-manifest.786f417039aa3c57cf67ed8952a71beb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
