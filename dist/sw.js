fbed0e752e00121ee20a115658991470b5051012
Jared Miller <jarmille@umflint.edu>
2019-08-10 17:09:50 -0400

    Path updates

--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.1671e662c75bc57d2bccf50c10ebb693.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.b2284472bad9ad0eda0ba70e76720d10.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
