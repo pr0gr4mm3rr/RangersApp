7f86d7bf3487999db8a8a7d0a2c42c9edbd40a38
Jared Miller <jarmille@umflint.edu>
2019-08-10 21:05:15 -0400

    Code updates

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
-importScripts("precache-manifest.b2284472bad9ad0eda0ba70e76720d10.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.3f720b8db72bfa968df5480d6720f43e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+
