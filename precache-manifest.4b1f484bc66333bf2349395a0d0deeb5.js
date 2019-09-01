d1a71276b17c339d92b539a4a2b3e8692e61ec44
pr0gr4mm3rr <pr0gr4mm3rr@gmail.com>
2019-09-01 16:31:13 -0400

    Build

---
+++ precache-manifest.4b1f484bc66333bf2349395a0d0deeb5.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc385f28ed9666909a1eba9e6ec7ab8",
+    "url": "assets/IndustryPrototypes.ts"
+  },
+  {
+    "revision": "f0295a17502592a32e4c",
+    "url": "index.bundle.js"
+  },
+  {
+    "revision": "e3f499791f5e0657f17e29cbba66d54a",
+    "url": "index.html"
+  }
+]);
